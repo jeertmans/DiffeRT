3e8e8345e4d044dd99885864e1a3b965af3d7a47
Jérome Eertmans <jeertmans@icloud.com>
2023-10-31 18:49:56 +0100

    chore(lib): use *batch

--- differt/rt/image_method.py
+++ differt/rt/image_method.py
@@ -11,10 +11,10 @@ from typeguard import typechecked as typechecker
 @jaxtyped
 @typechecker
 def image_of_vertices_with_respect_to_mirrors(
-    vertices: Float[Array, "N 3"],
-    mirror_vertices: Float[Array, "N 3"],
-    mirror_normals: Float[Array, "N 3"],
-) -> Float[Array, "N 3"]:
+    vertices: Float[Array, "*batch 3"],
+    mirror_vertices: Float[Array, "*batch 3"],
+    mirror_normals: Float[Array, "*batch 3"],
+) -> Float[Array, "*batch 3"]:
     """
     Return the image of vertices with respect to mirrors.
     """
--- tests/rt/test_image_method.py
+++ tests/rt/test_image_method.py
@@ -40,7 +40,7 @@ def test_image_of_vertices_with_respect_to_mirrors() -> None:
             (10, 3),
             pytest.raises(TypeError),
         ),
-        ((3,), (3,), (3,), pytest.raises(TypeError)),
+        ((3,), (3,), (3,), does_not_raise()),
     ],
 )
 @random_inputs("vertices", "mirror_vertices", "mirror_normals")
@@ -54,10 +54,33 @@ def test_image_of_vertices_with_respect_to_mirrors_random_inputs(
         got = image_of_vertices_with_respect_to_mirrors(
             vertices, mirror_vertices, mirror_normals
         )
-        for i in range(vertices.shape[0]):
-            incident = vertices[i, :] - mirror_vertices[i, :]
+        if len(vertices.shape) == 2:
+            for i in range(vertices.shape[0]):
+                incident = vertices[i, :] - mirror_vertices[i, :]
+                expected = (
+                    vertices[i, :]
+                    - 2.0
+                    * jnp.dot(incident, mirror_normals[i, :])
+                    * mirror_normals[i, :]
+                )
+                chex.assert_trees_all_close(got[i, :], expected, rtol=1e-5)
+        else:
+            incident = vertices - mirror_vertices
             expected = (
-                vertices[i, :]
-                - 2.0 * jnp.dot(incident, mirror_normals[i, :]) * mirror_normals[i, :]
+                vertices - 2.0 * jnp.dot(incident, mirror_normals) * mirror_normals
             )
-            chex.assert_trees_all_close(got[i, :], expected, rtol=1e-5)
+            chex.assert_trees_all_close(got, expected, rtol=1e-5)
+
+
+# def test_image_of_vertices_with_respect_to_mirrors() -> None:
+#    vertices = jnp.array([[+0.0, +0.0, +1.0], [+1.0, +2.0, +3.0]])
+#    expected = jnp.array([[+0.0, +0.0, -1.0], [+1.0, +2.0, -3.0]])
+#    mirror_vertex = jnp.array([0.0, 0.0, 0.0])
+#    mirror_normal = jnp.array([0.0, 0.0, 1.0])
+#    n = vertices.shape[0]
+#    mirror_vertices = jnp.tile(mirror_vertex, (n, 1))
+#    mirror_normals = jnp.tile(mirror_normal, (n, 1))
+#    got = image_of_vertices_with_respect_to_mirrors(
+#        vertices, mirror_vertices, mirror_normals
+#    )
+#    chex.assert_trees_all_close(got, expected)
