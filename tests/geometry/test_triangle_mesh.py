cdc463648964e2ada90bddcbf5ecfd0f2ed84bce
Jérome Eertmans <jeertmans@icloud.com>
2023-11-02 17:57:23 +0100

    chore(tests): add tests

--- tests/geometry/test_triangle_mesh.py
+++ tests/geometry/test_triangle_mesh.py
@@ -1,9 +1,14 @@
 from collections.abc import Iterator
 from pathlib import Path
 
+import chex
+import jax.numpy as jnp
 import pytest
 
-from differt.geometry import TriangleMesh
+from differt.geometry.triangle_mesh import (
+    TriangleMesh,
+    triangles_contain_vertices_assuming_inside_same_plane,
+)
 
 
 @pytest.fixture(scope="module")
@@ -14,6 +19,27 @@ def two_buildings_obj_file() -> Iterator[Path]:
 @pytest.fixture(scope="module")
 def two_buildings_mesh(two_buildings_obj_file: Path) -> Iterator[TriangleMesh]:
     yield TriangleMesh.load_obj(two_buildings_obj_file)
+
+
+def test_triangles_contain_vertices_assuming_inside_same_planes() -> None:
+    triangle_vertices = jnp.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+    vertices = jnp.array(
+        [
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.3, 0.3, 0.0],
+            [1.0, 1.0, 0.0],
+            [0.3, 0.3, 1.0],
+        ]
+    )
+    expected = jnp.array([True, True, True, True, False, False])
+    n = vertices.shape[0]
+    triangle_vertices = jnp.tile(triangle_vertices, (n, 1, 1))
+    got = triangles_contain_vertices_assuming_inside_same_plane(
+        triangle_vertices, vertices
+    )
+    chex.assert_trees_all_equal(got, expected)
 
 
 class TestTriangleMesh:
