2406d69e785869119b443163b098444e106bf5b1
Jérome Eertmans <jeertmans@icloud.com>
2023-11-09 17:40:26 +0100

    fix(deps): lockfile fixed

--- ok.py
+++
@@ -1,9 +0,0 @@
-# from differt.scene.scene import TriangleMeshScene
-#
-# scene = TriangleMeshScene.load_obj(
-#    "../DiffeRT-research/simulations/huawei/two_buildings.obj"
-# )
-#
-# scene.plot()
-#
-# print(scene.diffraction_edges)
