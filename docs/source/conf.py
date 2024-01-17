dfc398e6f8ca1f3f2f7d4f7cc55199d879cef662
Jérome Eertmans <jeertmans@icloud.com>
2024-01-17 15:52:55 +0100

    chore(docs): fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ apidoc_output_dir = "reference"
 apidoc_excluded_paths = ["conftest.py"]
 apidoc_separate_modules = True
 apidoc_toc_file = False
-apidoc_module_first = True
+apidoc_module_first = False
 apidoc_extra_args = ["--maxdepth=1", "--templatedir=source/_templates"]
 
 # -- OpenGraph settings
