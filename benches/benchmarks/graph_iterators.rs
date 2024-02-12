5323c002f80b7bcc3765b576111ec2f1815aa3df
Jérome Eertmans <jeertmans@icloud.com>
2024-02-12 11:08:51 +0100

    fix benchmarks

--- benches/benchmarks/graph_iterators.rs
+++ benches/benchmarks/graph_iterators.rs
@@ -1,16 +1,50 @@
-use criterion::{black_box, criterion_group, Criterion};
+use criterion::{black_box, criterion_group, Criterion, Throughput};
 
-use differt::rt::graph::{CompleteGraph, DiGraph, IntoAllPathsIterator};
+use differt::rt::graph::{complete::CompleteGraph, directed::DiGraph};
+
+const NUM_NODES: usize = 1000;
+const DIRECT_PATH: bool = true;
+const DEPTH: usize = 5;
+const INCLUDE_FROM_AND_TO: bool = false;
 
 fn di_graph_from_complete_graph_all_paths(c: &mut Criterion) {
+    let mut group = c.benchmark_group("di_graph_from_complete_graph_all_paths");
+    group.throughput(Throughput::Elements(1));
+    let mut graph: DiGraph = CompleteGraph::new(NUM_NODES).into();
+    let (from, to) = graph.insert_from_and_to_nodes(DIRECT_PATH);
 
-    let mut graph: DiGraph = CompleteGraph::new(1000).into();
-    let (from, to) = graph.insert_from_and_to_nodes(true);
+    let mut iter = graph
+        .all_paths(from, to, DEPTH, INCLUDE_FROM_AND_TO)
+        .cycle();
 
-    let mut iter = graph.all_paths(from, to, 5, false);
+    group.bench_function("di_graph_from_complete_graph_all_paths", |b| {
+        b.iter(|| black_box(iter.next()))
+    });
 
-    c.bench_function("di_graph_from_complete_graph_all_paths", || black_box(iter.next()));
+    group.finish();
 }
 
+fn di_graph_from_complete_graph_all_paths_array_chunks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("di_graph_from_complete_graph_all_paths_array_chunks");
+    let mut graph: DiGraph = CompleteGraph::new(NUM_NODES).into();
+    let (from, to) = graph.insert_from_and_to_nodes(DIRECT_PATH);
 
-criterion_group!(benches, di_graph_from_complete_graph_all_paths);
+    for chunk_size in [1, 10, 100, 1000] {
+        group.throughput(Throughput::Elements(chunk_size as u64));
+        let mut iter = graph
+            .all_paths_array_chunks(from, to, DEPTH, INCLUDE_FROM_AND_TO, chunk_size)
+            .cycle();
+
+        group.bench_function(format!("{chunk_size}"), |b| {
+            b.iter(|| black_box(iter.next()))
+        });
+    }
+
+    group.finish()
+}
+
+criterion_group!(
+    benches,
+    di_graph_from_complete_graph_all_paths,
+    di_graph_from_complete_graph_all_paths_array_chunks
+);
