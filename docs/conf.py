4371d91ae13dba7794fb57e4c1e335baf1766e03
Jérome Eertmans <jeertmans@icloud.com>
2024-02-22 14:10:05 +0100

    is it an extension"?????

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Jérome Eertmans'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_book_theme",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
