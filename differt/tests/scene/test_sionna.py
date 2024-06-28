7375ce1ce5a38bc2a89961c313195e500f9add26
Jérome Eertmans <jeertmans@icloud.com>
2024-06-28 17:59:37 +0200

    fix(tests): regex

--- differt/tests/scene/test_sionna.py
+++ differt/tests/scene/test_sionna.py
@@ -32,7 +32,7 @@ def test_download_sionna_scenes_existing_empty_folder(empty_folder: Path) -> Non
 
 
 def test_download_sionna_scenes_existing_non_empty_folder(folder: Path) -> None:
-    with pytest.raises(OSError, match="[Dd]irectory not empty"):
+    with pytest.raises(OSError, match="[Dd]irectory (is )?not empty"):
         download_sionna_scenes(folder=folder, cached=False)
 
 
