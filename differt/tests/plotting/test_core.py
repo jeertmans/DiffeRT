81a81b67ed302080f0acbe03320d0151d76c8944
Jérome Eertmans <jeertmans@icloud.com>
2024-06-28 11:50:50 +0200

    fix(lib): some typos

--- differt/tests/plotting/test_core.py
+++ differt/tests/plotting/test_core.py
@@ -1,7 +1,10 @@
+from contextlib import AbstractContextManager
+from contextlib import nullcontext as does_not_raise
+
 import numpy as np
 import pytest
 
-from differt.plotting import draw_mesh, use
+from differt.plotting import draw_image, draw_markers, draw_mesh, draw_paths, use
 
 
 @pytest.mark.parametrize(
@@ -15,3 +18,59 @@ def test_draw_mesh(
     triangles = np.array([[0, 1, 2], [0, 2, 3]], dtype=np.int32)
     with use(backend):
         _ = draw_mesh(vertices, triangles)
+
+
+@pytest.mark.parametrize(
+    "backend",
+    ("vispy", "matplotlib", "plotly"),
+)
+def test_draw_paths(
+    backend: str,
+) -> None:
+    paths = np.random.rand(10, 4, 3)
+    with use(backend):
+        _ = draw_paths(paths)
+
+
+@pytest.mark.parametrize(
+    "backend,expectation",
+    (
+        ("vispy", does_not_raise()),
+        ("matplotlib", pytest.raises(NotImplementedError)),
+        ("plotly", does_not_raise()),
+    ),
+)
+@pytest.mark.parametrize("with_labels", (True, False))
+def test_draw_markers(
+    backend: str, expectation: AbstractContextManager[Exception], with_labels: bool
+) -> None:
+    markers = np.random.rand(4, 3)
+
+    if with_labels:
+        labels = ["A", "B", "C", "D"]
+    else:
+        labels = None
+
+    with use(backend), expectation:
+        _ = draw_markers(markers, labels=labels)
+
+
+@pytest.mark.parametrize(
+    "backend",
+    ("vispy", "matplotlib", "plotly"),
+)
+@pytest.mark.parametrize(
+    "pass_xy",
+    (True, False),
+)
+def test_draw_image(
+    backend: str,
+    pass_xy: bool,
+) -> None:
+    x = np.linspace(0, 1, 10)
+    y = np.linspace(0, 1, 20)
+    X, Y = np.meshgrid(x, y)  # noqa: N806
+    data = X * Y
+
+    with use(backend):
+        _ = draw_image(data, x=x if pass_xy else None, y=y if pass_xy else None)
