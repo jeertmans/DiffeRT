d175d510badd6f811caa121f63c7f2f86a487204
Jérome Eertmans <jeertmans@icloud.com>
2024-09-16 15:53:35 +0200

    chore(tests): move sionna download to fixture

--- differt/tests/scene/test_triangle_scene.py
+++ differt/tests/scene/test_triangle_scene.py
@@ -1,30 +1,32 @@
+from pathlib import Path
+
 import equinox as eqx
 import jax.numpy as jnp
 import pytest
 
 from differt.scene.sionna import (
-    download_sionna_scenes,
     get_sionna_scene,
     list_sionna_scenes,
 )
 from differt.scene.triangle_scene import TriangleScene
 from differt_core.scene.sionna import SionnaScene
 
-download_sionna_scenes()
-
 
 class TestTriangleScene:
     @pytest.mark.parametrize("scene_name", list_sionna_scenes())
-    def test_load_xml(self, scene_name: str) -> None:
+    def test_load_xml(self, scene_name: str, sionna_folder: Path) -> None:
         # Sionne scenes are all triangle scenes.
-        file = get_sionna_scene(scene_name)
+        file = get_sionna_scene(scene_name, folder=sionna_folder)
         scene = TriangleScene.load_xml(file)
         sionna_scene = SionnaScene.load_xml(file)
 
         assert len(scene.meshes) == len(sionna_scene.shapes)
 
-    def test_plot(self) -> None:
-        file = get_sionna_scene("simple_street_canyon")
+    def test_plot(
+        self,
+        sionna_folder: Path,
+    ) -> None:
+        file = get_sionna_scene("simple_street_canyon", folder=sionna_folder)
         scene = TriangleScene.load_xml(file)
 
         tx = jnp.array([[0.0, 0.0, 0.0]])
