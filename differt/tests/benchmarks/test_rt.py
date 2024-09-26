41207e9bd8dcb80c4da55404f4c6feb024ca3d4d
Jérome Eertmans <jeertmans@icloud.com>
2024-09-26 19:11:43 +0200

    chore(tests): add new benchmarks

---
+++ differt/tests/benchmarks/test_rt.py
@@ -0,0 +1,60 @@
+import pytest
+import pytest
+from jaxtyping import PRNGKeyArray
+
+from differt.rt.fermat import fermat_path_on_planar_mirrors
+from differt.rt.image_method import (
+    image_method,
+)
+
+from ..rt.utils import PlanarMirrorsSetup
+from pytest_benchmark.fixture import BenchmarkFixture
+
+batches = pytest.mark.parametrize(
+    "batch",
+    [
+        (),
+        (10,),
+        (
+            10,
+            20,
+            30,
+        ),
+    ],
+)
+
+@pytest.mark.benchmark(group="image_method")
+@batches
+def test_image_method(
+    batch: tuple[int, ...],
+    basic_planar_mirrors_setup: PlanarMirrorsSetup,
+    key: PRNGKeyArray,
+    benchmark: BenchmarkFixture
+) -> None:
+    setup = basic_planar_mirrors_setup.broadcast_to(*batch).add_noeffect_noise(
+        key=key
+    )
+    _ = benchmark(lambda: image_method(
+        setup.from_vertices,
+        setup.to_vertices,
+        setup.mirror_vertices,
+        setup.mirror_normals,
+    ).block_until_ready())
+
+@pytest.mark.benchmark(group="fermat_method")
+@batches
+def test_fermat(
+    batch: tuple[int, ...],
+    basic_planar_mirrors_setup: PlanarMirrorsSetup,
+    key: PRNGKeyArray,
+    benchmark: BenchmarkFixture
+) -> None:
+    setup = basic_planar_mirrors_setup.broadcast_to(*batch).add_noeffect_noise(
+        key=key
+    )
+    _ = benchmark(lambda: fermat_path_on_planar_mirrors(
+        setup.from_vertices,
+        setup.to_vertices,
+        setup.mirror_vertices,
+        setup.mirror_normals,
+    ).block_until_ready())
--- differt/tests/rt/conftest.py
+++ differt/tests/rt/conftest.py
@@ -1,42 +1,4 @@
-import jax.numpy as jnp
-import pytest
-
-from .utils import PlanarMirrorsSetup
+__all__ = ("basic_planar_mirrors_setup",)
 
 
-@pytest.fixture(scope="session")
-def basic_planar_mirrors_setup() -> PlanarMirrorsSetup:
-    """
-    Test setup that looks something like:
-
-                1           3
-             ───────     ───────
-           0                           5
-    (from) x                           x (to)
-
-                   ───────      ───────
-                      2            4
-
-    where xs are starting and ending vertices, and '───────' are mirrors.
-    """
-    return PlanarMirrorsSetup(
-        from_vertices=jnp.array([0.0, 0.0, 0.0]),
-        to_vertices=jnp.array([1.0, 0.0, 0.0]),
-        mirror_vertices=jnp.array([
-            [0.0, +1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, +1.0, 0.0],
-            [0.0, -1.0, 0.0],
-        ]),
-        mirror_normals=jnp.array(
-            [[0.0, -1.0, 0.0], [0.0, +1.0, 0.0], [0.0, -1.0, 0.0], [0.0, +1.0, 0.0]],
-        ),
-        paths=jnp.array(
-            [
-                [1.0 / 8.0, +1.0, 0.0],
-                [3.0 / 8.0, -1.0, 0.0],
-                [5.0 / 8.0, +1.0, 0.0],
-                [7.0 / 8.0, -1.0, 0.0],
-            ],
-        ),
-    )
+from .fixtures import basic_planar_mirrors_setup
