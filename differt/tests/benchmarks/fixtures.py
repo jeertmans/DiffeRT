2a03c813a1c89ff0a55f0f83ecf793f9f7c85b6b
Jérome Eertmans <jeertmans@icloud.com>
2024-09-28 16:07:07 +0200

    perf(lib): unroll image method and `use_scan=True/False` (#119)
    * perf(lib): unroll image method and `use_scan=False`
    
    * feat(lib): add `use_scan` argument
    
    * fix(lib): correct axes
    
    * chore(ci): add `use_scan=False/True` tests
    
    * fix(tests): oops
    
    * fix(lib): enable scan by default as it seems better
    
    * chore(tests): smarter test for rt methods
    
    * chore(ci): add cuda check

---
+++ differt/tests/benchmarks/fixtures.py
@@ -0,0 +1,22 @@
+import jax
+import pytest
+
+from ..rt.utils import PlanarMirrorsSetup
+
+from jaxtyping import PRNGKeyArray
+
+
+@pytest.fixture
+def large_random_planar_mirrors_setup(key: PRNGKeyArray) -> PlanarMirrorsSetup:
+    num_mirrors = 3
+    num_path_candidates = 10_000
+
+    key_from, key_to, key_m_vertices, key_m_normals, key_paths = jax.random.split(key, 5)
+
+    from_vertices = jax.random.uniform(key_from, (3,))
+    to_vertices = jax.random.uniform(key_to, (3,))
+    mirror_vertices = jax.random.uniform(key_m_vertices, (num_path_candidates, num_mirrors, 3))
+    mirror_normals = jax.random.uniform(key_m_normals, (num_path_candidates, num_mirrors, 3))
+    paths = jax.random.uniform(key_paths, (num_path_candidates, num_mirrors, 3))
+
+    return PlanarMirrorsSetup(from_vertices, to_vertices, mirror_vertices, mirror_normals, paths)
