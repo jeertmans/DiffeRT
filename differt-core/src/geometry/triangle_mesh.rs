e626818651a403a42b45d1c79581b81a2324a1e4
Jérome Eertmans <jeertmans@icloud.com>
2024-09-30 12:37:18 +0200

    chore(docs): format Rust's docstring

--- differt-core/src/geometry/triangle_mesh.rs
+++ differt-core/src/geometry/triangle_mesh.rs
@@ -119,21 +119,24 @@ impl TriangleMesh {
 
 #[pymethods]
 impl TriangleMesh {
-    /// jaxtyping.Float[np.ndarray, 'num_vertices 3']: The array of triangle vertices.
+    /// jaxtyping.Float[np.ndarray, 'num_vertices 3']: The array of triangle
+    /// vertices.
     #[getter]
     fn vertices<'py>(&self, py: Python<'py>) -> Bound<'py, PyArray2<f32>> {
         let array = arr2(&self.vertices);
         PyArray2::from_owned_array_bound(py, array)
     }
 
-    /// jaxtyping.Int[np.ndarray, 'num_triangles 3']: The array of triangle indices.
+    /// jaxtyping.Int[np.ndarray, 'num_triangles 3']: The array of triangle
+    /// indices.
     #[getter]
     fn triangles<'py>(&self, py: Python<'py>) -> Bound<'py, PyArray2<usize>> {
         let array = arr2(&self.triangles);
         PyArray2::from_owned_array_bound(py, array)
     }
 
-    /// jaxtyping.Float[numpy.ndarray, 'num_vertices 3'] | None: The array of face colors.
+    /// jaxtyping.Float[numpy.ndarray, 'num_vertices 3'] | None: The array of
+    /// face colors.
     ///
     /// The array contains the face colors, as RGB triplets,
     /// with a black color used as defaults (if some faces have a color).
@@ -147,7 +150,8 @@ impl TriangleMesh {
         None
     }
 
-    /// jaxtyping.Int[numpy.ndarray, 'num_vertices'] | None: The array of face materials.
+    /// jaxtyping.Int[numpy.ndarray, 'num_vertices'] | None: The array of face
+    /// materials.
     ///
     /// The array contains the material indices,
     /// with a special placeholder value of :data:`-1`.
@@ -161,7 +165,8 @@ impl TriangleMesh {
         None
     }
 
-    /// jaxtyping.Int[numpy.ndarray, 'num_objects 2'] | None: The array of object indices.
+    /// jaxtyping.Int[numpy.ndarray, 'num_objects 2'] | None: The array of
+    /// object indices.
     ///
     /// If the present mesh contains multiple objects, usually as a result of
     /// appending multiple meshes together, this array contain start end end
