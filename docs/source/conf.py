3e12072fd5a3a365b49108bf31f59eb63c0421c9
Jérome Eertmans <jeertmans@icloud.com>
2024-10-31 16:36:44 +0100

    fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,7 +97,6 @@ ogp_use_first_image = True
 
 always_document_param_types = False
 always_use_bars_union = True
-autodoc_member_order = "bysource"  # We force class variables to appear first
 
 # -- MyST-nb settings
 myst_heading_anchors = 3
