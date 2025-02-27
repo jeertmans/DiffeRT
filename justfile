jeertmans/DiffeRT

commit c8a24a5d26bd5c97e718838f5c23067394668ea7

chore(deps): use `dependency-groups` from PEP735 (#221)
* chore(deps): use `dependency-groups` from PEP735

[PEP735](https://peps.python.org/pep-0735/) introduces dependency groups which are a better alternative to optional dependencies for non-packages.

* fix(ci): RTD

* chore(dev): format `pyproject.toml`

.readthedocs.yaml
@@ -15,7 +15,7 @@ build:
     - asdf plugin add uv
     - asdf install uv latest
     - asdf global uv latest
-    - uv pip install -r pyproject.toml --extra docs --no-cache --python $READTHEDOCS_VIRTUALENV_PATH
+    - uv sync --group docs --no-cache --python $READTHEDOCS_VIRTUALENV_PATH
 sphinx:
   builder: html
   configuration: docs/source/conf.py

.github/workflows/test.yml
@@ -92,7 +92,7 @@ jobs:
         path: differt/src/differt/scene/scenes
 
     - name: Run tests
-      run: uv run --python ${{ matrix.pyversion }} --resolution ${{ matrix.resolution }} --frozen --no-dev --extra tests-extended pytest
+      run: uv run --python ${{ matrix.pyversion }} --resolution ${{ matrix.resolution }} --frozen --no-dev --group tests-extended pytest
 
     - name: Upload to codecov.io
       uses: codecov/codecov-action@v5
@@ -187,7 +187,7 @@ jobs:
     - name: Install dependencies for profiling
       run: |
         rm -rf differt-core/python/differt_core/*.so
-        uv pip install -r pyproject.toml --extra tests \
+        uv pip install -r pyproject.toml --group tests \
         --python='${{ steps.setup-python.outputs.python-path }}' \
         --config-settings=build-args='--profile profiling' --reinstall-package differt_core
       env:
@@ -206,7 +206,7 @@ jobs:
     - name: Re-install dependencies
       run: |
         rm -rf differt-core/python/differt_core/*.so
-        uv pip install -r pyproject.toml --extra tests \
+        uv pip install -r pyproject.toml --group tests \
         --python='${{ steps.setup-python.outputs.python-path }}' \
         --config-settings=build-args='--profile profiling' --reinstall-package differt_core
       env:
@@ -262,7 +262,7 @@ jobs:
       uses: taiki-e/install-action@just
 
     - name: Install dependencies
-      run: just install --locked --no-dev --extra docs
+      run: just install --locked --no-dev --group docs
 
     - name: Check links in documentation
       run: just docs/linkcheck

pyproject.toml
@@ -1,13 +1,13 @@
-[project]
-authors = [{name = "Jérome Eertmans", email = "jeertmans@icloud.com"}]
-dependencies = []
-name = "differt-dev"
-requires-python = ">=3.10"
-version = "1.0.0"
-
-[project.optional-dependencies]
+[dependency-groups]
 cuda = [
   "jax[cuda]>=0.5.0",
+]
+dev = [
+  {include-group = "docs"},
+  {include-group = "tests"},
+  "bump-my-version>=0.20.3",
+  "maturin-import-hook>=0.2.0",
+  "pre-commit>=3.5.0",
 ]
 docs = [
   "differt[all]",
@@ -46,10 +46,17 @@ tests = [
   "scipy>=1.12.0",
 ]
 tests-extended = [
-  "differt-dev[tests]",
+  {include-group = "tests"},
   "open3d-cpu>=0.19.0;python_version<'3.13' and sys_platform=='linux'",
   "sionna==0.19.1;python_version<'3.12' and sys_platform=='linux'",
 ]
+
+[project]
+authors = [{name = "Jérome Eertmans", email = "jeertmans@icloud.com"}]
+dependencies = []
+name = "differt-dev"
+requires-python = ">=3.10"
+version = "1.0.0"
 
 [tool.bumpversion]
 allow_dirty = false
@@ -240,15 +247,9 @@ convention = "google"
 [tool.uv]
 conflicts = [
   [
-    {extra = "cuda"},
-    {extra = "tests-extended"},
+    {group = "cuda"},
+    {group = "tests-extended"},
   ],
-]
-dev-dependencies = [
-  "differt-dev[docs,tests]",
-  "bump-my-version>=0.20.3",
-  "maturin-import-hook>=0.2.0",
-  "pre-commit>=3.5.0",
 ]
 override-dependencies = [
   "tensorflow>=2.18,<2.19",  # To support NumPy 2

commit 01522b2a61f16b4fcb3e19fafb2d46c6148aad0c

fix(ci): bugs introduced by #221
Fix some bugs introduced by #221.

.pre-commit-config.yaml
@@ -80,3 +80,7 @@ repos:
     additional_dependencies:
     - tomli
     exclude_types: [jupyter]  # GitHub action will run on Notebooks
+- repo: https://github.com/astral-sh/uv-pre-commit
+  rev: 0.6.3
+  hooks:
+  - id: uv-lock

.readthedocs.yaml
@@ -15,7 +15,7 @@ build:
     - asdf plugin add uv
     - asdf install uv latest
     - asdf global uv latest
-    - uv sync --group docs --no-cache --python $READTHEDOCS_VIRTUALENV_PATH
+    - UV_PROJECT_ENVIRONMENT=$READTHEDOCS_VIRTUALENV_PATH uv sync --group docs --no-cache
 sphinx:
   builder: html
   configuration: docs/source/conf.py

docs/justfile
@@ -12,7 +12,7 @@ clean-autosummary:
 
 # Build docs
 build builder="html" *OPTIONS="-W --keep-going":
-    uv run --extra docs sphinx-build -M {{ builder }} source build {{ OPTIONS }}
+    uv run --group docs sphinx-build -M {{ builder }} source build {{ OPTIONS }}
 
 # Build docs in "draft" mode
 draft *ARGS:

justfile
@@ -14,7 +14,7 @@ bench: bench-python bench-rust
 [group('python')]
 [group('test')]
 bench-python *ARGS:
-    uv run --extra tests pytest -n0 --benchmark-enable --benchmark-only --jaxtyping-packages="" {{ ARGS }}
+    uv run --group tests pytest -n0 --benchmark-enable --benchmark-only --jaxtyping-packages="" {{ ARGS }}
 
 # Benchmark Rust code
 [group('rust')]
@@ -93,7 +93,7 @@ test: test-python test-rust
 [group('python')]
 [group('test')]
 test-python *ARGS:
-    uv run --extra tests pytest {{ ARGS }}
+    uv run --group tests pytest {{ ARGS }}
 
 # Test Rust code
 [group('rust')]