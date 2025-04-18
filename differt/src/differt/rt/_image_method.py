fix(lib): avoid NaNs in the gradient of the Image method
While we avoid returning the result of division by zero, it is still evaluated (because `jnp.where` executes both branches), resulting in a NaN output in the gradient of the intersection with planes tests, when rays are parallel to the planes. We want to avoid this, as it can cause issues in upstream gradient descends.

differt/src/differt/rt/_image_method.py
@@ -119,8 +119,13 @@ def intersection_of_rays_with_planes(
     # [*batch 1]
     vn = dot(v, plane_normals, keepdims=True)
 
-    t = vn / jnp.where(vn == 0.0, 1.0, un)
-    return ray_origins + ray_directions * jnp.where(u == 0.0, 1.0, t)
+    parallel = un == 0.0
+    un = jnp.where(parallel, 1.0, un)
+
+    t = jnp.where(vn == 0.0, vn, jnp.where(parallel, jnp.inf, vn / un))
+    return ray_origins + ray_directions * jnp.where(
+        (u == 0.0).all(axis=-1, keepdims=True), 1.0, t
+    )
 
 
 @jax.jit