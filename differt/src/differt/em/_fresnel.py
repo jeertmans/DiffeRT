fix(lib): avoid NaNs in the gradient of the Image method (#246)
* fix(lib): avoid NaNs in the gradient of the Image method and related

While we avoid returning the result of division by zero, it is still evaluated (because `jnp.where` executes both branches), resulting in a NaN output in the gradient of the intersection with planes tests, when rays are parallel to the planes. We want to avoid this, as it can cause issues in upstream gradient descends.

differt/src/differt/em/_fresnel.py
@@ -128,7 +128,7 @@ def fresnel_coefficients(
 
             >>> from differt.em import fresnel_coefficients
             >>>
-            >>> n = 1.5  # Air to glass
+            >>> n = 1.5 + 0j  # Air to glass
             >>> theta = jnp.linspace(0, jnp.pi / 2)
             >>> cos_theta = jnp.cos(theta)
             >>> (r_s, r_p), (t_s, t_p) = fresnel_coefficients(n, cos_theta)
@@ -155,7 +155,7 @@ def fresnel_coefficients(
 
             >>> from differt.em import fresnel_coefficients
             >>>
-            >>> n = 1/ 1.5  #  Glass to air
+            >>> n = 1 / 1.5 + 0j  #  Glass to air
             >>> theta = jnp.linspace(0, jnp.pi / 2, 300)
             >>> cos_theta = jnp.cos(theta)
             >>> (r_s, r_p), (t_s, t_p) = fresnel_coefficients(n, cos_theta)

differt/src/differt/scene/_triangle_scene.py
@@ -359,14 +359,17 @@ def _compute_paths_sbr(
         # [num_tx_vertices num_rays 3]
         mirror_normals = jnp.take(mesh.normals, triangles, axis=0)
 
+        # Mark rays leaving the scene as invalid
+        inside_scene = jnp.isfinite(t_hit)
+        valid_rays &= inside_scene
+        # And avoid creating NaNs
+        t_hit = jnp.where(inside_scene, t_hit, jnp.zeros_like(t_hit))
+
         ray_origins += t_hit[..., None] * ray_directions
         ray_directions = (
             ray_directions
             - 2.0 * dot(ray_directions, mirror_normals, keepdims=True) * mirror_normals
         )
-
-        # Mark rays leaving the scene as invalid
-        valid_rays &= jnp.isfinite(t_hit)
 
         return (ray_origins, ray_directions, valid_rays), (
             triangles,