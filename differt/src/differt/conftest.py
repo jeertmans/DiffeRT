5f24b9bb3ebb7ce6827c104a623057ef964714b5
Jérome Eertmans <jeertmans@icloud.com>
2025-05-08 11:00:49 +0200

    fix(tests): patch Equinox's `_wrap_method` (#257)
    See https://github.com/patrick-kidger/equinox/issues/1016

--- differt/src/differt/conftest.py
+++ differt/src/differt/conftest.py
@@ -2,6 +2,7 @@ import sys
 from typing import Any
 
 import chex
+import equinox as eqx
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
@@ -33,3 +34,19 @@ def set_printoptions() -> None:
     # We need to do that because floats seem to vary across OSes:
     # https://github.com/numpy/numpy/issues/21209.
     jax.numpy.set_printoptions(precision=7, suppress=True)
+
+
+class _wrap_method(eqx._module._wrap_method):  # noqa: N801, SLF001
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        try:  # noqa: SIM105
+            self.__module__ = self.method.__module__
+        except AttributeError:
+            pass
+        try:  # noqa: SIM105
+            self.__doc__ = self.method.__doc__
+        except AttributeError:
+            pass
+
+
+eqx._module._wrap_method = _wrap_method  # noqa: SLF001
