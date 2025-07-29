a96b129b5547fc8fb451fd22ce92d40da6de738a
Jérome Eertmans <jeertmans@icloud.com>
2025-07-29 13:41:27 +0200

    chore(tests): follow-up of #301
    Shortening benchmark names and adding tests for all ray-triangle tests.

--- differt/tests/benchmarks/test_rt.py
+++ differt/tests/benchmarks/test_rt.py
@@ -1,17 +1,30 @@
 from typing import Literal
 
+import equinox as eqx
 import jax.numpy as jnp
 import pytest
+from jaxtyping import PRNGKeyArray
 from pytest_codspeed import BenchmarkFixture
 
+from differt.geometry import fibonacci_lattice
 from differt.rt import (
     fermat_path_on_planar_mirrors,
+    first_triangles_hit_by_rays,
     image_method,
+    rays_intersect_any_triangle,
     triangles_visible_from_vertices,
 )
 from differt.scene._triangle_scene import TriangleScene
 
 from ..rt.utils import PlanarMirrorsSetup
+
+
+def random_scene(scene: TriangleScene, *, key: PRNGKeyArray) -> TriangleScene:
+    return eqx.tree_at(
+        lambda s: s.mesh,
+        scene,
+        scene.mesh.sample(0.5, by_masking=True, sample_objects=True, key=key),
+    )
 
 
 @pytest.mark.benchmark(group="image_method")
@@ -54,18 +67,68 @@ def test_fermat(
     _ = benchmark(bench_fun)
 
 
-@pytest.mark.benchmark(group="triangles_visible_from_vertices")
-def test_transmitter_visibility_in_simple_street_canyon_scene(
+@pytest.mark.benchmark(group="rays_intersect_any_triangle")
+def test_rays_intersect_any_triangle(
     simple_street_canyon_scene: TriangleScene,
     benchmark: BenchmarkFixture,
+    key: PRNGKeyArray,
 ) -> None:
-    scene = simple_street_canyon_scene
+    scene = random_scene(simple_street_canyon_scene, key=key)
+
+    ray_origins = scene.transmitters
+    ray_directions = fibonacci_lattice(1_000_000)
+
+    def bench_fun() -> None:
+        rays_intersect_any_triangle(
+            ray_origins,
+            ray_directions,
+            scene.mesh.triangle_vertices,
+            active_triangles=scene.mesh.mask,
+        ).block_until_ready()
+
+    bench_fun()
+
+    _ = benchmark(bench_fun)
+
+
+@pytest.mark.benchmark(group="triangles_visible_from_vertices")
+def test_transmitter_visibility(
+    simple_street_canyon_scene: TriangleScene,
+    benchmark: BenchmarkFixture,
+    key: PRNGKeyArray,
+) -> None:
+    scene = random_scene(simple_street_canyon_scene, key=key)
 
     def bench_fun() -> None:
         triangles_visible_from_vertices(
             scene.transmitters,
             scene.mesh.triangle_vertices,
+            active_triangles=scene.mesh.mask,
         ).block_until_ready()
+
+    bench_fun()
+
+    _ = benchmark(bench_fun)
+
+
+@pytest.mark.benchmark(group="first_triangles_hit_by_rays")
+def test_first_triangles_hit_by_rays(
+    simple_street_canyon_scene: TriangleScene,
+    benchmark: BenchmarkFixture,
+    key: PRNGKeyArray,
+) -> None:
+    scene = random_scene(simple_street_canyon_scene, key=key)
+
+    ray_origins = scene.transmitters
+    ray_directions = fibonacci_lattice(1_000_000)
+
+    def bench_fun() -> None:
+        first_triangles_hit_by_rays(
+            ray_origins,
+            ray_directions,
+            scene.mesh.triangle_vertices,
+            active_triangles=scene.mesh.mask,
+        )[0].block_until_ready()
 
     bench_fun()
 
@@ -74,12 +137,13 @@ def test_transmitter_visibility_in_simple_street_canyon_scene(
 
 @pytest.mark.benchmark(group="compute_paths")
 @pytest.mark.parametrize("method", ["exhaustive", "hybrid"])
-def test_compute_paths_in_simple_street_canyon_scene(
+def test_compute_paths(
     method: Literal["exhaustive", "hybrid"],
     simple_street_canyon_scene: TriangleScene,
     benchmark: BenchmarkFixture,
+    key: PRNGKeyArray,
 ) -> None:
-    scene = simple_street_canyon_scene.set_assume_quads()
+    scene = random_scene(simple_street_canyon_scene.set_assume_quads(), key=key)
 
     def bench_fun() -> None:
         num_valid_paths = jnp.array(0, dtype=jnp.int32)
