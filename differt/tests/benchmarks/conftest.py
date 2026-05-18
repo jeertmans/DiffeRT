8d5e6bf3d8bd77a87ceffcf2f3c51774c8fbafd6
Jérome Eertmans <jeertmans@icloud.com>
2026-05-18 10:13:43 +0200

    fix

--- differt/tests/benchmarks/conftest.py
+++ differt/tests/benchmarks/conftest.py
@@ -1,11 +1,12 @@
 __all__ = (
     "bench_scene",
     "large_random_planar_mirrors_setup",
+    "simple_street_canyon_scene",
     "sionna_folder",
 )
 
 
-from ..scene.fixtures import sionna_folder
+from ..scene.fixtures import simple_street_canyon_scene, sionna_folder
 from .fixtures import (
     bench_scene,
     large_random_planar_mirrors_setup,
